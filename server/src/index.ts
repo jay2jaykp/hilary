2af54d794ce97f23764f8d67d120d3edbbdc2122
jay.patel5 <jay.patel5@loblaw.ca>
2023-03-21 20:16:18 -0400

    added api endpoints

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,80 @@
 import express from "express";
+import { prisma } from "./utils/prisma";
 
 const app = express();
-const PORT = 5000;
+const PORT = 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+// add middleware that can parse the json data in request body
+app.use(express.json());
+
+// API Endpoints for our CRUD operations
+
+// 1. Fetch ALL ToDos
+app.get("/", async (req, res) => {
+  // const createTodo = await prisma.todo.create({
+  //   data: {
+  //     title: "my second todo",
+  //     completed: true,
+  //   },
+  // });
+  const allTodo = await prisma.todo.findMany();
+  res.send(allTodo);
 });
 
-app.listen(5000, () => {
+// 2. Fetch specific ToDo
+// params
+app.get("/:id", async (req, res) => {
+  const Todo = await prisma.todo.findUnique({
+    where: {
+      id: Number(req.params.id),
+    },
+  });
+
+  res.send(Todo);
+});
+
+// 3. Delete a specific Todo
+app.delete("/:id", async (req, res) => {
+  const deleteTodo = await prisma.todo.delete({
+    where: {
+      id: Number(req.params.id),
+    },
+  });
+
+  res.send(deleteTodo);
+});
+
+// 4. Update existing Todo
+app.post("/:id", async (req, res) => {
+  const updatedTitle = req.body.title;
+  const updateTodo = await prisma.todo.update({
+    where: {
+      id: Number(req.params.id),
+    },
+    data: {
+      title: updatedTitle,
+    },
+  });
+
+  res.send(updateTodo);
+});
+
+// 5. Update the completion value
+app.post("/completed/:id", async (req, res) => {
+  console.log("I am updating completed column");
+  const incomingValue = req.body.completed;
+  const updateTodo = await prisma.todo.update({
+    where: {
+      id: Number(req.params.id),
+    },
+    data: {
+      completed: incomingValue,
+    },
+  });
+
+  res.send(updateTodo);
+});
+
+app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
